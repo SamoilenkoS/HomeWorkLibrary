640ecc9c937b1de2ba684646eeaf44a17efa7089
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2021-11-03 18:41:35 +0200

    Add project files.

---
+++ HomeWorkLibrary/VariableHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWorkLibrary
+{
+    public class VariableHelper
+    {
+        public static double Task1(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
